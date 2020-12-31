02792a10fa4502e8ba62d05a75725a6cdd22bfc2
Gerald Conde <gerald.conde1@gmail.com>
2020-12-31 01:27:02 -0500

    Use latest gradle. Update Windows test script.

--- test.bat
+++ test.bat
@@ -1,3 +1,7 @@
 SET PATH=%PATH%;x64\Release
 
-x64\Release\test.exe test\resources\trip1.yaml test\resources\trip2.yaml
+if exist x64\Release\test.exe (
+    x64\Release\test.exe test\resources\trip1.yaml test\resources\trip2.yaml
+) else (
+    test\build\install\main\release\windows\test.bat test\resources\trip1.yaml test\resources\trip2.yaml
+)
